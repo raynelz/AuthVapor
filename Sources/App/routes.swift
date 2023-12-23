b8cfd29ceb935b9531b59f0f419f29ce7c4d5b84
Zahar Litvinchuk <97597336+raynelz@users.noreply.github.com>
2023-12-23 19:30:54 +0300

    basic routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -9,6 +9,32 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+    
+    app.get("hello", "vapor") { req -> String in
+        return "Hello, Vapor!"
+        
+    }
+    
+    app.get("hello", ":name") { req -> String in
+        guard let name = req.parameters.get("name") else {
+            throw Abort(.internalServerError)
+        }
+        
+        return "Hello, \(name)!"
+    }
+    
+    app.post("info") { req -> InfoResponse in
+        let data = try req.content.decode(InfoData.self)
+        return InfoResponse(request: data)
+    }
 
     try app.register(collection: TodoController())
+}
+
+struct InfoData: Content {
+    let name: String
+}
+
+struct InfoResponse: Content {
+    let request: InfoData
 }
